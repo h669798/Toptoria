85fb0f25a4ec26ffb45b061e381ff75a02c2cad6
Elena N.J <elena.nicole.jacobsen@hotmail.com>
2022-09-14 06:26:27 +0200

    Nightly update.

---
+++ Toptoria Unity Prosjekt/Assets/Scripts/BallMove.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour
+{  
+   
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Space))
+       {
+        GetComponent<Animator>().Play("ElsaAnna");
+       }
+    }
+
+   
+}
